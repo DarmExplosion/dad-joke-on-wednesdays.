3b4dffaa104bce786928f2338289187dfeb4a47e
DarmExplosion <87719681+DarmExplosion@users.noreply.github.com>
2022-09-02 19:38:24 +0200

    Create index.py

---
+++ index.py
@@ -0,0 +1,43 @@
+from time import sleep
+import requests
+import json
+from datetime import datetime
+import calendar
+from datetime import date
+
+url = "WEBHOOK URL" 
+
+data = {
+    "content" : "Sup",
+    "username" : "Hooka"
+}
+
+newhead = {
+    "Accept" : "application/json"
+}
+
+while True:
+    d = date.today()
+    print('Date is:', d)
+    z = calendar.day_name[d.weekday()]
+    print('Weekday name is:', z)
+
+    if z == "Wednesday":
+        print(datetime.now().weekday())
+        x = requests.get("https://icanhazdadjoke.com/", headers = newhead)
+        print(x.content)
+        y = json.loads(x.content)
+        print(y["joke"])
+        data = {
+            "content" : y["joke"],
+            "username" : "Hooka"
+        }
+        result = requests.post(url, json = data)
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
+        else:
+            print("Payload delivered successfully, code {}.".format(result.status_code))
+    sleep(100)
+    
